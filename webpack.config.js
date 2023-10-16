bf3d0ac3d4d7e632c1e27c959fb437693bd02e7c
Vadim Marchenko <v3.marchenko@itransition.com>
2023-10-16 15:26:17 +0300

    Add webpack; configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('node:path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const baseConfig = {
+  devtool: 'source-map',
+  mode: 'production',
+  entry: {
+    index: './src/js/app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/images',
+          to: './images',
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+};
+
+module.exports = ({ mode }) => {
+  return mode === 'development' ? merge(baseConfig, {
+    devtool: 'inline-source-map',
+    mode: 'development',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, '../dist'),
+      },
+      compress: true,
+      port: 9000,
+    },
+  }) : baseConfig;
+};
