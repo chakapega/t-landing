9cc46629d804f573ab059b2ec6a3d265a98d7eca
Vadim Marchenko <v3.marchenko@itransition.com>
2023-10-18 12:21:50 +0300

    Change logic of mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
 import '../scss/style.scss';
+
+const headerHamburgerMenuButton = document.querySelector('.header__hamburger-menu-button');
+const headerHamburgerMenuIcon = document.querySelector('.header__hamburger-menu-icon');
+const headerHamburgerMenuCloseIcon = document.querySelector('.header__hamburger-menu-close-icon');
+const headerMobileMenuWrapper = document.querySelector('.header__mobile-menu-wrapper');
+const main = document.querySelector('.main');
+
+headerHamburgerMenuButton.addEventListener('click', () => {
+  headerHamburgerMenuIcon.classList.toggle('header__hamburger-menu-icon_hidden');
+  headerHamburgerMenuCloseIcon.classList.toggle('header__hamburger-menu-close-icon_hidden');
+  headerMobileMenuWrapper.classList.toggle('header__mobile-menu-wrapper_hidden');
+  main.classList.toggle('main_hidden');
+})
